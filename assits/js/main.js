55484711a099cce120aae7c833048c52fb283514
Rawanqawareeq <Rawanqawareeq95@gmail.com>
2024-08-27 15:05:47 +0300

    update js

--- assits/js/main.js
+++ assits/js/main.js
@@ -1,87 +1,89 @@
-const form = document.querySelector(".home-taskform");
-const p    = document.querySelector(".p-task");
-const pDate = document.querySelector(".p-date");
-const homeTask_list = document.querySelector(".homeTask_list");
-const listOfTask = [];
-form.onsubmit = (e)=>{
-    e.preventDefault(); 
-    const task = e.target.elements;
-    let taskDate = new Date(task["task-date"].value);
-    let currentDate = new Date(new Date().toISOString().split("T")[0]);  
-        if(task["home-task"].value.trim() == ""){
-           p.style.color = 'red';
-           p.textContent = "home-task is empty"
-           p.style.display ="inline-block";  
-           return;
-        }
-        if(task["home-task"].value.trim() != ""){
-            p.style.display = "none";   
-            return;
-        }
-        if(task["task-date"].value.trim() == ""){
-            pDate.style.color = 'red';
-            pDate.textContent = "Date is empty";
-            pDate.style.display ="inline-block"; 
-            return;
-        }
-        if(taskDate < currentDate) {
-            pDate.style.color = 'red';
-            pDate.textContent = "Date is expired";
-            pDate.style.display = "inline-block"; 
-            return;
-        }
-        if(task["task-date"].value.trim() != ""){
-            pDate.style.display ="none";    
-            return;
-        } 
-        const  home_task  = task["home-task"].value;
-        const  task_date  = task["task-date"].value;
-        listOfTask.push({home_task,task_date,completed:false});
+const form = document.querySelector('.home-taskform');
+const errorName = document.querySelector('.task-name-error');
+const errorDate = document.querySelector('.task-date-error');
+const homeTask = document.querySelector(".task-list");
+
+const listOfTask =[];
+let flagName = false;
+let flagDate = false;
+
+form.addEventListener('submit',(e)=>{
+     e.preventDefault();
+     const task = e.target.elements;
+     let dateTask = new Date(task["task-date"].value);
+     let currentDate = new Date(new Date().toISOString().split("T")[0]);
+     if(dateTask < currentDate ){
+        errorDate.textContent = "Date is expired";
+        errorDate.classList.add("error");
+        errorDate.classList.remove("d-none");
+        flagDate = false;
+     }
+     if(task["task-date"].value == ""){
+        errorDate.textContent = "Date is empty";
+        errorDate.classList.add("error");
+        errorDate.classList.remove("d-none");
+        flagDate = false;
+     }
+     if(task["task-name"].value == ""){
+        errorName.textContent = "Task is empty";
+        errorName.classList.add("error");
+        errorName.classList.remove("d-none");
+        flagName = false;
+     }
+     if(task["task-name"].value != ""){
+        errorName.classList.remove("error");
+        errorName.classList.add("d-none");
+        flagName = true;
+     }
+     if(task["task-date"].value.trim() != "" && dateTask >= currentDate ){
+        errorDate.classList.remove("error");
+        errorDate.classList.add("d-none");
+        flagDate = true;
+    }
+    if(flagDate && flagName){
+        listOfTask.push({TaskName:task["task-name"].value,dateTask,checked:false})
         sortTask();
-        displayTasks();
-        task["home-task"].value = '';
-        task["task-date"].value = '';
-}
+        task["task-date"].value = "";
+        task["task-name"].value="";
+    }
+    displayTasks();
+})
 function sortTask(){
-    listOfTask.sort((a, b) => new Date(a.task_date) - new Date(b.task_date));
+    listOfTask.sort((a, b) => new Date(a.dateTask) - new Date(b.dateTask));
 }
-
 function displayTasks(filter = 'all'){
-    homeTask_list.innerHTML = '';
+    homeTask.innerHTML = '';
     const filteredHomework = listOfTask.filter((ele)=>{
-        if(filter == 'completed') return ele.completed;
-        if(filter == 'pending') return   !ele.completed;
-        return true;
+      if(filter == 'completed') return ele.checked;
+      if(filter == 'pending')   return !ele.checked;
+      return true;
+  });
+  filteredHomework.forEach((element,index) => {
+      const homeList = document.createElement('li');
+      homeList.className =`homework-item ${element.checked ? 'completed' : ''} `;
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      checkbox.checked = element.checked;
+      checkbox.addEventListener('change',()=>{
+         element.checked = checkbox.checked;
+         displayTasks();
+      })
+      const span = document.createElement('span');
+      span.textContent = `${element.TaskName} ${element.dateTask.toISOString().split("T")[0]}`
+      const btnRemove = document.createElement('button');
+      btnRemove.textContent = "Remove";
+      btnRemove.className = "btn_Remove";
+      btnRemove.addEventListener('click',()=>{
+         listOfTask.splice(index,1);
+         displayTasks(filter = 'all');
+      })
+      homeList.appendChild(checkbox);
+      homeList.appendChild(span);
+      homeList.appendChild(btnRemove);
+      homeTask.appendChild(homeList);
+      document.querySelector('.all').addEventListener('click', () => displayTasks('all'));
+      document.querySelector('.completed').addEventListener('click', () => displayTasks('completed'));
+      document.querySelector('.pending').addEventListener('click', () => displayTasks('pending'));
     });
-    filteredHomework.forEach((ele,index)=>{
-        const listitem = document.createElement('li');
-        listitem.className = `homework-item ${ele.completed ? 'completed' :''}`;
-        const checkbox  = document.createElement('input');
-        checkbox.type ='checkbox';
-        checkbox.checked = ele.completed;
-        checkbox.addEventListener('change',function(){
-            ele.completed = checkbox.checked;
-            console.log(checkbox.checked);
-            displayTasks(filter);
-        });
-        const span = document.createElement('span');
-        span.textContent = `${ele.home_task}  ${ele.task_date}`;
-        const removeBtn = document.createElement('button');
-        removeBtn.textContent = 'Delete';
-        removeBtn.addEventListener('click',function(){
-            listOfTask.splice(index, 1);
-            displayTasks(filter);
-        });
-        listitem.appendChild(checkbox);
-        listitem.appendChild(span);
-        listitem.appendChild(removeBtn);
-        homeTask_list.appendChild(listitem);
-        document.querySelector('.all').addEventListener('click', () => displayTasks('all'));
-        document.querySelector('.completed').addEventListener('click', () => displayTasks('completed'));
-        document.querySelector('.pending').addEventListener('click', () => displayTasks('pending'));
-    })
-   
+    
 }
-
-
-   
